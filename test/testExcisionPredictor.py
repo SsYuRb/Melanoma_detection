c815f8201a7d02fe76deb222a89ef172f4815ab3
Gan4x4 <gan4x4@gmail.com>
2023-11-22 00:14:43 +0300

    Add test for unscaled images

--- test/testExcisionPredictor.py
+++ test/testExcisionPredictor.py
@@ -1,17 +1,28 @@
 import unittest
-from src.ExcisionPredictor import ExcisionPredictor, draw_mask_contour
+from src.ExcisionPredictor import ExcisionPredictor, draw_mask_contour, get_contour
 import cv2
+
 
 class ExcisionPredictorTestCase(unittest.TestCase):
     def test_base(self):
-
-        ep = ExcisionPredictor()
         image = cv2.imread("data/qr.jpg")
         mask = cv2.imread("data/mask.png")
-        dilated_mask = ep(image,mask)
-        draw_mask_contour(image,dilated_mask)
+
+        ep = ExcisionPredictor()
+        image, dilated_mask = ep(image, mask)
+        contour = get_contour(dilated_mask)
+        draw_mask_contour(image, dilated_mask)
+        self.assertEqual(contour[0].shape[0], 926)
         cv2.imwrite("test_base.png", image)
-        #self.assertGreater(len(contours) ,0)
+
+    def test_no_resize(self):
+        image = cv2.imread("data/qr.jpg")
+        mask = cv2.imread("data/mask.png")
+
+        ep = ExcisionPredictor(work_width=None,  # disable rescaling
+                               margin=2)  # for speedup
+        image, dilated_mask = ep(image, mask)
+        self.assertEqual(mask.shape, dilated_mask.shape)
 
 
 if __name__ == '__main__':
